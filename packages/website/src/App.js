323aa8acbe9e50b8ae6ccbec84c2f7dd1ebacb3c
Hjalti Kolbeinsson <hjalti@kolbeinsson.se>
2017-06-20 11:16:06 +0000

    eslint

--- packages/website/src/App.js
+++ packages/website/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.scss';
+import React from 'react'
+import logo from './logo.svg'
+import styles from './App.scss'
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <div className={styles["App-header"]}>
-          <img src={logo} className={styles["App-logo"]} alt="logo" />
-          <h2>Welcome to React</h2>
+const App = () =>
+    (<div className={styles.App}>
+        <div className={styles['App-header']}>
+            <img src={logo} className={styles['App-logo']} alt="logo" />
+            <h2>Welcome to React</h2>
         </div>
-        <p className={styles["App-intro"]}>
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className={styles['App-intro']}>
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
-}
+    </div>)
 
-export default App;
+export default App
