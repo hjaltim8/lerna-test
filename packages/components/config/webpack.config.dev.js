249130b67083b2ce5629b36a7b89f27e7280caca
Hjalti Kolbeinsson <hjalti@kolbeinsson.se>
2017-06-20 11:09:26 +0000

    Added sass with css loaders, autoprefixer and classnames/classnames-loader

--- packages/components/config/webpack.config.dev.js
+++ packages/components/config/webpack.config.dev.js
@@ -137,6 +137,7 @@ module.exports = {
           /\.html$/,
           /\.(js|jsx)$/,
           /\.css$/,
+          /\.scss$/,
           /\.json$/,
           /\.bmp$/,
           /\.gif$/,
@@ -180,11 +181,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
+          'classnames-loader',
           require.resolve('style-loader'),
           {
             loader: require.resolve('css-loader'),
             options: {
               importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]',
             },
           },
           {
@@ -205,6 +209,45 @@ module.exports = {
               ],
             },
           },
+        ],
+      },
+      // "postcss" loader applies autoprefixer to our CSS.
+      // "css" loader resolves paths in CSS and adds assets as dependencies.
+      // "style" loader turns CSS into JS modules that inject <style> tags.
+      // In production, we use a plugin to extract that CSS to a file, but
+      // in development "style" loader enables hot editing of CSS.
+      {
+        test: /\.scss$/,
+        use: [
+          'classnames-loader',
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 2,
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]',
+            },
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+          { loader: require.resolve('sass-loader')},
         ],
       },
       // ** STOP ** Are you adding a new loader?
