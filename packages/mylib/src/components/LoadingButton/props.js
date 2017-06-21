9da5ee7c6dd73820a3caeb27c9104dc6685f004b
Hjalti Kolbeinsson <hjalti@kolbeinsson.se>
2017-06-21 15:50:41 +0000

    removed nwb created mylib

--- packages/mylib/src/components/LoadingButton/props.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types'
-
-const { bool, string } = PropTypes
-
-export const propTypes = { disabled: bool, loading: bool, type: string }
-export const defaultProps = { disabled: false, loading: false, type: 'button' }
--- packages/mylib/src/index.js
+++
@@ -1,1 +0,0 @@
-export { default as LoadingButton } from './components/LoadingButton'
